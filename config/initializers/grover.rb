6b93c8ae2d39c44bf36a04ec2c0241bbaab83388
briri <brian.riley@ucop.edu>
2024-06-21 05:08:34 -0700

    fix for fonts and margins in grover pdfs

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -8,38 +8,26 @@
 #   > export CHROMIUM_PATH=/apps/dmp/install/dmptool/.cache/puppeteer/chrome/linux-126.0.6478.61/chrome-linux64/chrome
 #   > export GROVER_NO_SANDBOX=true
 
+# Install fonts manually to /user/share/fonts
+# To view fonts > fc-list
 
 
 Grover.configure do |config|
   config.options = {
     format: 'letter',
-    # margin: {
-    #   top: '25px',
-    #   right: '25px',
-    #   bottom: '25px',
-    #   left: '25px'
-    # },
-    # prefer_css_page_size: true,
-    # emulate_media: 'screen',
-    # bypass_csp: true,
-    # media_features: [{ name: 'prefers-color-scheme', value: 'dark' }],
-    # timezone: 'Australia/Sydney',
+    prefer_css_page_size: true, # Tells Puppeteer to prefer the `@page` CSS directive for margins
     vision_deficiency: 'deuteranopia',
-    # extra_http_headers: { 'Accept-Language': 'en-US' },
-    # geolocation: { latitude: 59.95, longitude: 30.31667 },
-    # focus: '#some-element',
-    # hover: '#another-element',
-    # cache: true,
+    cache: true,
     timeout: 0, # Timeout in ms. A value of `0` means 'no timeout'
     request_timeout: 10000, # Timeout when fetching the content (overloads the `timeout` option)
     convert_timeout: 10000, # Timeout when converting the content (overloads the `timeout` option, only applies to PDF conversion)
 
-    headless: true,
+    headless: true, # Tell Puppeteer to use headless Chrome
     executable_path: Rails.root.join('.cache', 'puppeteer', 'chrome', 'linux-126.0.6478.61', 'chrome-linux64', 'chrome'),
 
     launch_args: ['--font-render-hinting=medium', '--no-sandbox', '--disable-setuid-sandbox'],
     wait_until: 'networkidle0',
     full_page: true,
-    devtools: true
+    # devtools: true
   }
 end

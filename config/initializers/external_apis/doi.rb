bf1d214f58968b98dac88562825ba50b1254189a
briri <brian.riley@ucop.edu>
2024-11-05 07:25:08 -0800

    added missing doi_ prefixed config vars to anyway config file

--- config/initializers/external_apis/doi.rb
+++ config/initializers/external_apis/doi.rb
@@ -6,9 +6,9 @@
 # `app/services/external_apis/doi.rb` to communicate with their API.
 #
 # To disable thiis feature, simply set 'active' to false
-Rails.configuration.x.dmproadmap.doi.landing_page_url = 'https://my.doi.org/'
-Rails.configuration.x.dmproadmap.doi.api_base_url = 'https://my.doi.org/api/'
-Rails.configuration.x.dmproadmap.doi.auth_path = 'auth_path'
-Rails.configuration.x.dmproadmap.doi.heartbeat_path = 'heartbeat'
-Rails.configuration.x.dmproadmap.doi.mint_path = 'doi'
-Rails.configuration.x.dmproadmap.doi.active = false
+Rails.configuration.x.dmproadmap.doi_landing_page_url = 'https://my.doi.org/'
+Rails.configuration.x.dmproadmap.doi_api_base_url = 'https://my.doi.org/api/'
+Rails.configuration.x.dmproadmap.doi_auth_path = 'auth_path'
+Rails.configuration.x.dmproadmap.doi_heartbeat_path = 'heartbeat'
+Rails.configuration.x.dmproadmap.doi_mint_path = 'doi'
+Rails.configuration.x.dmproadmap.doi_active = false

b8102b51e696641579b09ec8070488f17705347b
briri <brian.riley@ucop.edu>
2026-02-26 10:19:33 -0800

    update to fix issue with visibility form. fix issue with docker compose. bump dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -848,7 +848,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_06_21_160500) do
     t.index ["user_id"], name: "index_users_perms_on_user_id"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "plans"
   add_foreign_key "answers", "questions"
